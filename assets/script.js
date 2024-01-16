6c874f22e58440c111cb0786c864c72a4fb8c8bf
Testing Wizard <marcosmunozr15@gmail.com>
2024-01-15 18:29:20 -0600

    Added first draft of rules

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,36 @@
 
 var button = document.getElementById("generate");
 
+var defaultLength = 16;
+var upperCase = true;
+var lowerCase = true;
+var numbers = true;
+var symbols = true;
+
+
+ // Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword(length, upperCase, lowerCase, numbers, symbols) {
+
+  
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+}
+
+writePassword(defaultLength, upperCase, lowerCase, numbers, symbols);
+
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
 
 button.addEventListener("click", function() {
     var userInput;
-    var defaultLength = 16;
-    var upperCase = true;
-    var lowerCase = true;
-    var numbers = true;
-    var symbols = true;
 
     while (true) {
       var userInput = prompt("Please type a number to generate a password!\n\n- Type '1' if you need to set specific criteria to be included in the password (special characters, lowercase/uppercase, etc.)\n\n- Type '2' to generate a random strong password");
@@ -34,23 +56,4 @@ button.addEventListener("click", function() {
 });
 
 
-
-
-
-
-
- // Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
 
